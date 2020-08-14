6609b2c9411015de35a1a64cf165f50f53b8af72
Helson <hxs1024@163.com>
2020-08-14 16:55:30 +0800

    获取类的实例方法和对象方法的相关API。

--- objc4-756.2/objc4-756.2/objc-debug/main.m
+++ objc4-756.2/objc4-756.2/objc-debug/main.m
@@ -11,6 +11,12 @@
 #import <malloc/malloc.h>
 #import "Object.h"
 
+
+/**
+ 获取类的成员变量和属性
+
+ @param cls 类
+ */
 void testObjc_copyIvar_copyProperies(Class cls) {
     unsigned int count = 0;
     Ivar *ivars = class_copyIvarList(cls, &count);
@@ -37,15 +43,98 @@ void testObjc_copyIvar_copyProperies(Class cls) {
     
 }
 
+
+/**
+ 获取类的对象方法名
+
+ @param cls 类
+ */
+void testObjc_copyMethodList(Class cls) {
+    unsigned int count = 0;
+    Method *methods = class_copyMethodList(cls, &count);
+    for (unsigned int i=0; i < count; i++) {
+        Method const method = methods[i];
+        //获取方法名
+        NSString *key = NSStringFromSelector(method_getName(method));
+        
+        NSLog(@"Method, name: %@", key);
+    }
+    free(methods);
+}
+
+
+/**
+ 获取元类的实例方法
+ 类对象的类方法，在元类的方法列表里面。
+ @param cls 类
+ */
+void testInstanceMethod_classToMetaclass(Class cls) {
+    const char *className = class_getName(cls);
+    Class metaClass = objc_getMetaClass(className);
+    
+    Method method1 = class_getInstanceMethod(cls, @selector(walk));
+    Method method2 = class_getInstanceMethod(metaClass, @selector(fly));
+    
+    Method method3 = class_getInstanceMethod(cls, @selector(walk));
+    Method method4 = class_getInstanceMethod(metaClass, @selector(fly));
+    
+    NSLog(@"%p-%p-%p-%p",method1,method2,method3,method4);
+    NSLog(@"%s",__func__);
+}
+
+
+
+/**
+ 获取元类的类方法
+
+ @param cls 类
+ */
+void testClassMethod_classToMetaclass(Class cls) {
+    const char *className = class_getName(cls);
+    Class metaClass = objc_getMetaClass(className);
+    
+    Method method1 = class_getClassMethod(cls, @selector(walk));
+    Method method2 = class_getClassMethod(metaClass, @selector(fly));
+    
+    Method method3 = class_getClassMethod(cls, @selector(walk));
+    Method method4 = class_getClassMethod(metaClass, @selector(fly));
+    
+    NSLog(@"%p-%p-%p-%p",method1,method2,method3,method4);
+    NSLog(@"%s",__func__);
+}
+
+
+/**
+ 获取元类方法的实现。
+
+ @param cls 类
+ */
+void testIMP_classToMetaclass(Class cls) {
+    const char *className = class_getName(cls);
+    Class metaClass = objc_getMetaClass(className);
+    
+    IMP imp1 = class_getMethodImplementation(cls, @selector(walk));
+    IMP imp2 = class_getMethodImplementation(metaClass, @selector(fly));
+    
+    IMP imp3 = class_getMethodImplementation(cls, @selector(walk));
+    IMP imp4 = class_getMethodImplementation(metaClass, @selector(fly));
+    
+    NSLog(@"%p-%p-%p-%p",imp1,imp2,imp3,imp4);
+    NSLog(@"%s",__func__);
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         HXSPerson *p = [HXSPerson alloc];
         
         Class cls = object_getClass(p);
-        testObjc_copyIvar_copyProperies(cls);
-        
-        
+//        testObjc_copyIvar_copyProperies(cls);
+//        testObjc_copyMethodList(cls);
+//        testInstanceMethod_classToMetaclass(cls);
+//        testClassMethod_classToMetaclass(cls);
+//        testIMP_classToMetaclass(cls);
         NSLog(@"Class Name:%@", cls);
 
     }
